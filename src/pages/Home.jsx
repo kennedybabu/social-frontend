741560354dcffcbfb23ade8c9f577fc72c4ac270
kennedybabu <kennedynjogu8@gmail.com>
2022-07-07 12:29:06 +0300

    feature: fetch items from the api

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 const Home = () => {
+
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    getNotes()
+  }, [])
+
+  let getNotes = async () => {
+    let response = await fetch('http://127.0.0.1:8000/posts/')
+    let data = await response.json()
+    setPosts(data)
+    console.log('posts', posts)
+  }
+
   return (
     <div>
         <h2>Home Page</h2>
+
     </div>
   )
 }
